694ecd52ed29385e1de86d39e31bba7660af7448
culhaci22 <erenculhaci@hotmail.com>
2024-12-28 00:03:30 +0300

    Added notification menu, style changes for header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import "../styles/Header.css"; // Optional: Add your styles here
+import "../styles/Header.css";
+import React, { useState, useEffect, useRef } from "react";
 import bitescoutLogo11 from "../assets/bitescout-logo1-1.png";
 import notification from "../assets/ios-notification.png";
 import { Button } from "./ButtonWithImageAndText";
@@ -6,14 +7,41 @@ import profile from "../assets/Untitled-1.png";
 import { ImgButton } from "./ButtonWithImage";
 import globe from "../assets/globe.png";
 import "../styles/Button.css";
+import NotificationList from "./NotificationList";
 
 const Header = () => {
-  function notify(msg: string) {
-    alert(msg);
-  }
+  const [isNotificationMenuOpen, setIsNotificationMenuOpen] = useState(false);
+  const notificationMenuRef = useRef<HTMLDivElement>(null);
+
+  const notify = (msg: string) => alert(msg);
+
+  const toggleNotificationMenu = () => {
+    setIsNotificationMenuOpen((prevState) => !prevState);
+  };
+
+  // Close the menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        notificationMenuRef.current &&
+        !notificationMenuRef.current.contains(event.target as Node)
+      ) {
+        setIsNotificationMenuOpen(false);
+      }
+    };
+
+    if (isNotificationMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isNotificationMenuOpen]);
+
   return (
     <header className="header">
-      <img className="header__logo" src={bitescoutLogo11}></img>
+      <img className="header__logo" src={bitescoutLogo11} alt="Logo" />
       <nav className="header__nav">
         <Button
           class="button__"
@@ -25,8 +53,8 @@ const Header = () => {
         <ImgButton
           class="button__onlyimg"
           src={notification}
-          func={notify}
-          msg="notif"
+          func={toggleNotificationMenu}
+          msg=""
         />
         <ImgButton
           class="button__onlyimg"
@@ -35,6 +63,15 @@ const Header = () => {
           msg="language"
         />
       </nav>
+      {/* Notification Menu */}
+      <div
+        ref={notificationMenuRef}
+        className={`notification-menu ${
+          isNotificationMenuOpen ? "open" : ""
+        }`}
+      >
+        <NotificationList />
+      </div>
     </header>
   );
 };
